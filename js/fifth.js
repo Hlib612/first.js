ca07b36ca31c21ea3bfc58316479fa22eb4b1e42
Hlib612 <glebmalonos@gmail.com>
2025-02-22 11:25:37 +0200

    фвв

---
+++ js/fifth.js
@@ -0,0 +1,44 @@
+// const a = 4;
+// const b = 5;
+// // let result;
+// // if (a + b < 4) {
+// //   result = 'Нижче';
+// // } else {
+// //   result = 'Вище';
+// // }
+// const result = a + b < 4 ? 'Нижче' : 'Вище';
+
+// console.log(result)
+// Використовуючи тернарний оператор, напишіть код, що запитує:
+//  ‘Яка “офіційна” назва JavaScript?’
+//   Якщо відвідувач вводить “ECMAScript”, виведіть “Правильно!”, 
+//   в іншому випадку — виведіть: “Ви не знаєте? ECMAScript!”
+// const userInput = prompt('Яка “офіційна” назва JavaScript?');
+// const qestion = userInput === 'ECMAScript' ? 'Правильно!' : 'Ви не знаєте? ECMAScript!';
+// alert(qestion);
+
+// const field1 = prompt();
+// const field2 = prompt();
+// // if(field1 && field2){
+// // alert( "Обидва поля заповнені")
+// // } else {
+// //     alert("Не всі поля заповнені") 
+// // }
+// const qestion1 = field1 && field2 ? alert( "Обидва поля заповнені") : alert("Не всі поля заповнені");
+
+// const number1f = prompt();
+// const number2f = prompt();
+// const number1t = Number(number1f);
+// console.log(number1t)
+// const number2t = Number(number2f);
+// console.log(number2t)
+// const allcost = number1t + number2t;
+// console.log(allcost)
+// // if(allcost > 10){
+// //     alert("Сума більша за 10");
+// //     } else if(allcost === 10){
+// //         alert("Сума дорівнює 10");
+// //     } else {
+// //         alert("Сума менша 10") ;
+// //     }
+// const porivnana = allcost > 10 ? "Сума більша за 10" : "Сума менша або дорівнює 10";
