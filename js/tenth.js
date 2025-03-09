ddaf035092c548e8bbf639dd48208b94b549b733
Hlib612 <glebmalonos@gmail.com>
2025-03-09 11:21:41 +0200

    add

---
+++ js/tenth.js
@@ -0,0 +1,29 @@
+// const array = [10, 25, 13, 44, 15];
+// // Вивести в консоль всі елементи масиву за допомогою 
+// // циклів for та for…of.
+// for(const num of array){
+//     console.log(num);
+// }
+
+// 2
+// Порахувати загальну суму покупок в корзині за допомогою 
+// циклів for та for…of.
+
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+// let total = 0;
+
+// for(let i = 0; i < cart.length; i += 1){
+//     console.log(cart[i]);
+//     total = total + cart[i];
+//     console.log(total);
+// }
+
+// for(const num of cart){
+//     total = total + num;
+// }
+// console.log(total);
+
+// for(let i = 0; i < cart.length; i += 1){
+//     cart[i] -= cart[i] * 0.1
+//     total += cart[i];
+// }
