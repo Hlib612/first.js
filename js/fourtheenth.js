591a4b9627f1a758e596a62069184fa734138950
Hlib612 <glebmalonos@gmail.com>
2025-03-22 11:45:34 +0200

    add

---
+++ js/fourtheenth.js
@@ -0,0 +1,46 @@
+// const hallu = function(){
+//     return "hallu javascript"
+// }
+// console.log(hallu());
+
+
+// 2. 
+// Напишіть функцію hello2(),
+//  яка при виклику буде приймати змінну name (наприклад, «Василь») 
+//  і виводити рядок (в нашому випадку «Привіт, Василь»).
+
+// const hello = function(){
+//     return `hello ${name}`
+// }
+// let name = prompt();
+// console.log(hello());
+
+
+/* 3
+Оголоси функцію calculateTotalPrice, яка рахує загальну суму покупки. 
+Вона приймає два параметри, 
+
+orderedQuantity - перший параметр, кількість одиниць товару в замовленні;
+pricePerItem - другий параметр, ціна одиниці товару.
+Функція повинна повертати загальну суму покупки - 
+результат множення кількості товарів на ціну одного.
+
+Виклик calculateTotalPrice(5, 100) повертає 500 */
+
+// const calculateTotalPrice = function(orderedQuantity, pricePerItem){
+// return orderedQuantity * pricePerItem;
+// }
+// console.log(calculateTotalPrice(6, 10))
+
+// 4. 
+// Напишіть функцію mul(n, m), яка приймає два аргументи
+//  і повертає добуток, суму і різницю цих аргументів. 
+//  Перевірте її роботу.
+// const mul = function(n, m){
+//     const sum = n + m;
+//     const minus = n - m;
+//     const multiplay = n * m;
+//     return [sum, minus, multiplay];
+// }
+// console.log(mul(10, 5))
+
