aae40804eaf29f682fecea275ba8295bb49cd914
Hlib612 <glebmalonos@gmail.com>
2025-05-31 11:28:05 +0300

    add

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -12,3 +12,29 @@ console.log(linkEl);
 //  - Додайте клас "site-nav__link" до першого посилання
 //  - Збільшіть розмір шрифта посилання з атрибутом href = "https://"
 linkEl.classList.add("crazy")
+
+
+
+const colorPickerOptions = [
+    { label: 'red', color: '#F44336' },
+    { label: 'green', color: '#4CAF50' },
+    { label: 'blue', color: '#2196F3' },
+    { label: 'grey', color: '#607D8B' },
+    { label: 'pink', color: '#E91E63' },
+    { label: 'indigo', color: '#3F51B5' },
+  ];
+
+
+  const containerEl = document.querySelector(".js-color-picker");
+console.log(containerEl);
+const items = colorPickerOptions.map(option => {
+    console.log(option);
+    const button = document.createElement("button");
+    button.textContent = option.label;
+    button.classList.add('color-picker__option');
+    button.style.backgroundColor = option.color;
+    console.log(button);
+    return button;
+});
+console.log(items);
+containerEl.append(...items);
