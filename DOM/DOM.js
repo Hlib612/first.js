43786e1a491f80655166ea7b352e9a750cd44a85
Hlib612 <glebmalonos@gmail.com>
2025-06-07 11:27:00 +0300

    add

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -13,29 +13,26 @@
 // //  - Збільшіть розмір шрифта посилання з атрибутом href = "https://"
 // linkEl.classList.add("crazy")
 
+// const colorPickerOptions = [
+//     { label: 'red', color: '#F44336' },
+//     { label: 'green', color: '#4CAF50' },
+//     { label: 'blue', color: '#2196F3' },
+//     { label: 'grey', color: '#607D8B' },
+//     { label: 'pink', color: '#E91E63' },
+//     { label: 'indigo', color: '#3F51B5' },
+//   ];
 
-
-const colorPickerOptions = [
-    { label: 'red', color: '#F44336' },
-    { label: 'green', color: '#4CAF50' },
-    { label: 'blue', color: '#2196F3' },
-    { label: 'grey', color: '#607D8B' },
-    { label: 'pink', color: '#E91E63' },
-    { label: 'indigo', color: '#3F51B5' },
-  ];
-
-
-  const containerEl = document.querySelector(".js-color-picker");
-console.log(containerEl);
-const items = colorPickerOptions.map(option => {
-    console.log(option);
-    const button = document.createElement("button");
-    button.textContent = option.label;
-    button.classList.add('color-picker__option');
-    button.style.backgroundColor = option.color;
-    console.log(button);
-    return button;
-});
+//   const containerEl = document.querySelector(".js-color-picker");
+// console.log(containerEl);
+// const items = colorPickerOptions.map(option => {
+//     console.log(option);
+//     const button = document.createElement("button");
+//     button.textContent = option.label;
+//     button.classList.add('color-picker__option');
+//     button.style.backgroundColor = option.color;
+//     console.log(button);
+//     return button;
+// });
 // console.log(items);
 // containerEl.append(...items);
 
@@ -49,15 +46,40 @@ const items = colorPickerOptions.map(option => {
 // // додаємо в список
 // list.innerHTML = markup;
 
+// const markup = colorPickerOptions.map(option => {
+//   return `<button class="colorPickerOption" style="background-color: ${option.color};">${option.label}</button>`
+// }).join("")
+
+// containerEl.innerHTML = markup;
+
+// console.log(markup);
 
 
 
-const markup = colorPickerOptions.map(option => {
-  return `<button class="colorPickerOption" style="background-color: ${option.color};">${option.label}</button>`
-}).join("")
 
-containerEl.innerHTML = markup;
+//? 1.Написати функцію яка буде створювати карточку продукту.
+//? Додати карточку в DOM за допомогою методу createElement
+{
+  /* <article class="product">
+        <h2 class="product__name">Назва</h2>
+        <p class="product__descr">Опис</p>
+        <p product__pridct>Ціна: 1111 кредитів</p>
+    </article>
+    <div class="js-products"></div> */
+}
 
-console.log(markup);
+const productContainer = document.querySelector(".js-product");
+console.log(productContainer);
+const article = document.createElement("article");
+console.log(article);
+productContainer.append(article);
 
+// 2 спосіб innerHTML
+const cardContent = ` <h2 class="product__name">Назва</h2>
+        <p class="product__descr">Опис</p>
+        <p product__pridct>Ціна: 1111 кредитів</p> `;
 
+        article.innerHTML = cardContent;
+
+// 3 спосіб elem.insertAdjacentHTML(position, string);
+article.insertAdjacentHTML("afterbegin" , cardContent)
